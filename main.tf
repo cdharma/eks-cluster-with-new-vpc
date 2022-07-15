630843bda0e86695cc293b0f92e2e57ff30dbefb
Chetan Dharma <dcodercd@gmail.com>
2022-07-15 15:20:13 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,271 @@
+provider "aws" {
+  region = local.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_blueprints.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {}
+data "aws_caller_identity" "current" {}
+
+locals {
+  name = basename(path.cwd)
+  # var.cluster_name is for Terratest
+  cluster_name = coalesce(var.cluster_name, local.name)
+  region       = "us-west-2"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  #---------------------------------------------------------------
+  # ARGOCD ADD-ON APPLICATION
+  #---------------------------------------------------------------
+
+  addon_application = {
+    path               = "chart"
+    repo_url           = "https://github.com/cdharma/eks-blueprints-add-ons.git"
+    add_on_application = true
+  }
+
+  #---------------------------------------------------------------
+  # ARGOCD WORKLOAD APPLICATION
+  #---------------------------------------------------------------
+
+  workload_application = {
+    path               = "envs/dev"
+    repo_url           = "https://github.com/cdharma/eks-blueprints-workloads.git"
+    add_on_application = false
+  }
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+  }
+}
+
+#---------------------------------------------------------------
+# EKS Blueprints
+#---------------------------------------------------------------
+module "eks_blueprints" {
+  source = "../.."
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.22"
+
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnets
+
+  managed_node_groups = {
+    mg_5 = {
+      node_group_name = "managed-ondemand"
+      instance_types  = ["m5.large"]
+      min_size        = 2
+      subnet_ids      = module.vpc.private_subnets
+    }
+  }
+
+  platform_teams = {
+    admin = {
+      users = [
+        data.aws_caller_identity.current.arn
+      ]
+    }
+  }
+
+  application_teams = {
+    team-riker = {
+      "labels" = {
+          "appName"     = "riker-team-app",
+          "projectName" = "project-riker",
+          "environment" = "dev",
+          "domain"      = "example",
+          "uuid"        = "example",
+          "billingCode" = "example",
+          "branch"      = "example"
+        }
+        "quota" = {
+          "requests.cpu"    = "10000m",
+          "requests.memory" = "20Gi",
+          "limits.cpu"      = "20000m",
+          "limits.memory"   = "50Gi",
+          "pods"            = "10",
+          "secrets"         = "10",
+          "services"        = "10"
+        }
+        ## Manifests Example: we can specify a directory with kubernetes manifests that can be automatically applied in the team-riker namespace.
+        # manifests_dir = "./manifests-team-red"
+        users         = [data.aws_caller_identity.current.arn]
+      }
+
+    team-tfc = {
+      "labels" = {
+          "appName"     = "riker-team-app",
+          "projectName" = "project-riker",
+          "environment" = "dev",
+          "domain"      = "example",
+          "uuid"        = "example",
+          "billingCode" = "example",
+          "branch"      = "example"
+        }
+        "quota" = {
+          "requests.cpu"    = "10000m",
+          "requests.memory" = "20Gi",
+          "limits.cpu"      = "20000m",
+          "limits.memory"   = "50Gi",
+          "pods"            = "10",
+          "secrets"         = "10",
+          "services"        = "10"
+        }
+        ## Manifests Example: we can specify a directory with kubernetes manifests that can be automatically applied in the team-riker namespace.
+        # manifests_dir = "./manifests-team-red"
+        users         = [data.aws_caller_identity.current.arn]
+      }
+    }
+
+    tags = local.tags
+}
+
+module "aws_controllers" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.0.7/modules/kubernetes-addons"
+
+  eks_cluster_id = module.eks_blueprints.eks_cluster_id
+
+  #---------------------------------------------------------------
+  # Use AWS controllers separately for clearity
+  #---------------------------------------------------------------
+
+  enable_aws_load_balancer_controller = true
+  enable_karpenter                    = true
+  enable_aws_for_fluentbit            = false
+
+  depends_on = [module.eks_blueprints.managed_node_groups]
+}
+
+module "kubernetes-addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.0.7/modules/kubernetes-addons"
+
+  eks_cluster_id = module.eks_blueprints.eks_cluster_id
+
+  #---------------------------------------------------------------
+  # ARGO CD ADD-ON
+  #---------------------------------------------------------------
+
+  enable_argocd         = true
+  argocd_manage_add_ons = true # Indicates that ArgoCD is responsible for managing/deploying Add-ons.
+  argocd_applications = {
+    addons    = local.addon_application
+    workloads = local.workload_application
+  }
+
+  argocd_helm_config = {
+    values = [templatefile("${path.module}/argocd-values.yaml", {}),
+    templatefile("${path.module}/prom-values.yaml", {})
+    
+    ]
+  }
+
+  #---------------------------------------------------------------
+  # ADD-ONS - You can add additional addons here
+  # https://aws-ia.github.io/terraform-aws-eks-blueprints/add-ons/
+  #---------------------------------------------------------------
+
+  enable_aws_for_fluentbit            = false
+  enable_cert_manager                 = false
+  enable_cluster_autoscaler           = false
+  enable_ingress_nginx                = false
+  enable_keda                         = false
+  enable_metrics_server               = false
+  enable_prometheus                   = true
+  enable_traefik                      = false
+  enable_vpa                          = false
+  enable_yunikorn                     = false
+  enable_argo_rollouts                = false
+
+  depends_on = [module.eks_blueprints.managed_node_groups,module.aws_controllers]
+}
+
+
+
+/* module "eks_blueprints_kubernetes_addons" {
+  source = "../../modules/kubernetes-addons"
+
+  eks_cluster_id       = module.eks_blueprints.eks_cluster_id
+  eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
+  eks_oidc_provider    = module.eks_blueprints.oidc_provider
+  eks_cluster_version  = module.eks_blueprints.eks_cluster_version
+
+  # EKS Managed Add-ons
+  enable_amazon_eks_vpc_cni    = true
+  enable_amazon_eks_coredns    = true
+  enable_amazon_eks_kube_proxy = true
+
+  # Add-ons
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  enable_cluster_autoscaler           = true 
+  enable_aws_cloudwatch_metrics       = true
+
+  tags = local.tags
+} */
+
+#---------------------------------------------------------------
+# Supporting Resources
+#---------------------------------------------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  # Manage so we can name
+  manage_default_network_acl    = true
+  default_network_acl_tags      = { Name = "${local.name}-default" }
+  manage_default_route_table    = true
+  default_route_table_tags      = { Name = "${local.name}-default" }
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${local.name}-default" }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+
+  tags = local.tags
+}
